5634d8973b22609607d8cd96ccfad39e4e50acdf
Andreas Sandberg <andreas.sandberg1.as@gmail.com>
2024-02-19 19:29:40 +0000

    Write async function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,18 @@
-B1x-OK2qQMxv1h6XHdSp4DNuxGE
+const API_KEY = "B1x-OK2qQMxv1h6XHdSp4DNuxGE";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+async function getStatus(e) {
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        console.log(data.expiry);
+    }
+}
+
